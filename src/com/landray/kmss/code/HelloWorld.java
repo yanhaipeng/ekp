67190968ca45ff5bca9e60f88f11916b7300f768
yanhp <lxyanfei@126.com>
2016-03-23 00:02:57 +0800

    修改注释

--- src/com/landray/kmss/code/HelloWorld.java
+++ src/com/landray/kmss/code/HelloWorld.java
@@ -3,7 +3,7 @@ package com.landray.kmss.code;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// 在Eclipse添加注释
+		// 在Eclipse添加注释(World)
 		System.out.println("Hello world!");
 	}
 
