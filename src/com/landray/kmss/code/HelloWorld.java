aad08ffe9d9adbdedc3792007182dfef341bf47a
yanhaipeng <lxyanfei@126.com>
2016-03-22 23:57:58 +0800

    by Yan

--- src/com/landray/kmss/code/HelloWorld.java
+++ src/com/landray/kmss/code/HelloWorld.java
@@ -3,7 +3,7 @@ package com.landray.kmss.code;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// 在Eclipse添加注释
+		// 在Eclipse添加注释(by Yan)
 		System.out.println("Hello world!");
 	}
 
